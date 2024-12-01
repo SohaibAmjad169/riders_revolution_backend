10951f32d059be7ebd86903ff800fed1e5928dbc
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-01 19:45:18 +0500

    basic setup done

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import { Port } from './Config/Keys.js'
 import { MongoConnect } from './Config/MongoConfig.js'
+import { BikeRouter } from './Router/BikeRouter.js'
+import { UserRouter } from './Router/UserRouter.js'
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 MongoConnect()
-
+app.use('/Api/Bike', BikeRouter)
+app.use('/Api/User', UserRouter)
 app.listen(Port, () => {
   console.log(`CONNECTED ON ${Port}`)
 })
