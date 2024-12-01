b2b5074f03dfbe913dbf3d52d7f96559d48839be
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-02 03:09:09 +0500

    getting all the cart items , if page refreshed or something

---
+++ Controllers/Cart/GetCart.js
@@ -0,0 +1,20 @@
+import { Cart } from '../../Model/CartSchema.js'
+
+export const getCartByUserId = async (req, res) => {
+  const { userId } = req.body
+
+  try {
+    // Find the user's cart
+    const cart = await Cart.findOne({ userId }).populate('bikes.bikeId')
+
+    if (!cart) {
+      return res.status(404).json({ error: 'Cart not found.' })
+    }
+
+    // Return the cart details
+    return res.status(200).json({ cart })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: 'Internal server error' })
+  }
+}
