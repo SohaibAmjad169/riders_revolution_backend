691a4ab51ea723f2399f5ac6c5c01d26d19ef3f2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-01 19:45:52 +0500

    singout a user

---
+++ Controllers/Auth/SignOut.js
@@ -0,0 +1,14 @@
+import { signOut } from 'firebase/auth'
+import { Auth } from '../../Config/FireBaseConfig.js'
+export const Signout = async (req, res) => {
+  try {
+    // Sign out the user using Firebase Auth
+    await signOut(Auth)
+
+    // If successful, send a 200 response
+    res.status(200).json({ message: 'Successfully signed out', success: true })
+  } catch (error) {
+    // Catch and return any errors during the signout process
+    res.status(500).json({ message: error.message, success: false })
+  }
+}
