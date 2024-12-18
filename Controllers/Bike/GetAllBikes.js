d5399262a46303fcae2776b4e8299df5b1341387
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-19 03:26:13 +0500

    Update GetAllBikes.js

--- Controllers/Bike/GetAllBikes.js
+++ Controllers/Bike/GetAllBikes.js
@@ -2,22 +2,7 @@ import { Bike } from '../../Model/BikeModel.js'
 import { User } from '../../Model/UserModal.js'
 
 export const GetBikes = async (req, res) => {
-  const { Email } = req.body
-
   try {
-    // Step 1: Validate email
-    if (!Email) {
-      return res
-        .status(400)
-        .json({ error: 'Email is required to fetch bikes.' })
-    }
-
-    // Step 2: Check if the user exists
-    const userFound = await User.findOne({ Email })
-    if (!userFound) {
-      return res.status(404).json({ error: 'User not found.' })
-    }
-
     // Step 3: Fetch all bikes
     const allBikes = await Bike.find()
 
