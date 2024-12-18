b5e4e7d43c6383011ab04c3a587e59599f9bbf58
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-19 03:24:44 +0500

    getting cart details

--- Controllers/Order/GetSingleOrder.js
+++ Controllers/Order/GetSingleOrder.js
@@ -1,16 +1,22 @@
+import { Cart } from '../../Model/CartSchema.js'
 import Order from '../../Model/OrderModel.js'
+
 export const GetSingleOrder = async (req, res) => {
-  const { id } = req.query
+  const { id } = req.query // Extract the order ID from query parameters
+
   try {
-    // Find the user's Order
-    const SingleOrder = await Order.find({ _id: id })
+    // Find the order by ID and populate the cart details
+    const SingleOrder = await Order.findById(id)
     if (!SingleOrder) {
       return res.status(404).json({ error: 'Order not found.' })
     }
-    // Return the Order details
-    return res.status(200).json({ Order })
+    const GetCart = await Cart.findById('6763353a603c9fd69456f506').populate(
+      'bikes.bikeId'
+    )
+    // Return the order details along with the populated cart details
+    return res.status(200).json({ Order: SingleOrder, Cart: GetCart })
   } catch (error) {
-    console.error(error)
+    console.error('Error fetching single order:', error.message)
     return res.status(500).json({ error: 'Internal server error' })
   }
 }
