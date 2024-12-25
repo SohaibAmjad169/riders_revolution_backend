ef458f9ad7b9cf64b07fee7da4c1f3c2438e56b1
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-26 03:31:51 +0500

    adding services in cart

--- Model/CartSchema.js
+++ Model/CartSchema.js
@@ -34,6 +34,33 @@ const CartSchema = new mongoose.Schema(
         },
       },
     ],
+    services: [
+      {
+        serviceId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Service',
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        imageUrl: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+          min: 1,
+        },
+      },
+    ],
     totalPrice: {
       type: Number,
       required: true,
